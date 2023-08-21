92dca8a13d1cee8f209b8bb48a017ed1680f2c89
Rahat <rahatsarker40@gmail.com>
2023-08-21 21:51:31 +0600

    report

--- Transport-licensing-tax-management-main/Transport_licensing_tax_management/Areas/User/Controllers/TAXController.cs
+++ Transport-licensing-tax-management-main/Transport_licensing_tax_management/Areas/User/Controllers/TAXController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using Transport_licensing_tax_management.Data;
+using Transport_licensing_tax_management.Migrations;
 using Transport_licensing_tax_management.ViewModel;
 
 namespace Transport_licensing_tax_management.Areas.User.Controllers
@@ -49,6 +50,60 @@ namespace Transport_licensing_tax_management.Areas.User.Controllers
 
             ViewBag.TaxesHistory = taxHistory;
            
+            return View(taxHistory);
+        }
+
+        public IActionResult TaxAllReport(string CarNumber)
+        {
+            var vehicle = _context.Vehicles.FirstOrDefault(v => v.VehiclesNumber == CarNumber);
+
+            if (vehicle == null)
+            {
+                ViewBag.msg = "Car Not Found!";
+                return View();
+            }
+            var taxHistory = _context.Taxes
+            .Join(_context.Payments,
+                t => t.PaymentsID,
+                p => p.PaymentsID,
+                (t, p) => new TaxesVM
+                {
+                    taxID = t.taxID,
+                    VehiclesNumber = t.VehiclesNumber,
+                    Fees = p.Amount,
+                    Issu_Date = t.Issu_Date,
+                    Expired_Date = t.Expired_Date,
+                    RegisterNID = vehicle.RegisterNID
+                })
+            .Where(t => t.VehiclesNumber == CarNumber)
+            .ToList();
+
+            ViewBag.TaxesHistory = taxHistory;
+
+            return View(taxHistory);
+        }
+        public IActionResult Print(string CarNumber)
+        {
+            var vehicle = _context.Vehicles.FirstOrDefault(v => v.VehiclesNumber == CarNumber);
+
+            var taxHistory = _context.Taxes
+            .Join(_context.Payments,
+                t => t.PaymentsID,
+                p => p.PaymentsID,
+                (t, p) => new TaxesVM
+                {
+                    taxID = t.taxID,
+                    VehiclesNumber = t.VehiclesNumber,
+                    Fees = p.Amount,
+                    Issu_Date = t.Issu_Date,
+                    Expired_Date = t.Expired_Date,
+                    RegisterNID = vehicle.RegisterNID
+                })
+            .Where(t => t.VehiclesNumber == CarNumber)
+            .ToList();
+
+            ViewBag.TaxesHistory = taxHistory;
+
             return View(taxHistory);
         }
     }
